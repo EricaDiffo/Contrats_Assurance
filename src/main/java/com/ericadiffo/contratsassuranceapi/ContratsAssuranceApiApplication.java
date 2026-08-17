81e27a82063ebdf9f5bcfe84536a54a0be1376a8
EricaDiffo <diffoerica@icloud.com>
2026-08-17 15:43:58 +0200

    feat: configuration PostgreSQL via Docker Compose

---
+++ src/main/java/com/ericadiffo/contratsassuranceapi/ContratsAssuranceApiApplication.java
@@ -0,0 +1,13 @@
+package com.ericadiffo.contratsassuranceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContratsAssuranceApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContratsAssuranceApiApplication.class, args);
+    }
+
+}
